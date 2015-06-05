56ab280e61f6592508906a810f30ed8ec951f368
Dat Chu <dattanchu@gmail.com>
2015-06-05 11:32:06 -0500

    Grunt file correctly compile html files to templates.js

---
+++ www/js/templates.js
@@ -0,0 +1,11 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["www/template/_listItem.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '';
+
+}
+return __p
+};
