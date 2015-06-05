a33cfcefc39b9b2caae370fcc55da021d1a6b20d
Dat Chu <dattanchu@gmail.com>
2015-06-05 12:14:20 -0500

    More customization. Cannot seem to get jquery js to work

--- www/js/templates.js
+++ www/js/templates.js
@@ -4,7 +4,13 @@ this["JST"]["www/template/_listItem.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '';
+__p += '<li><a>\n    <h2>' +
+((__t = ( message.title )) == null ? '' : __t) +
+'</h2>\n    <p>' +
+((__t = ( message.blurb )) == null ? '' : __t) +
+'</p>\n    <p class="ul-li-aside">' +
+((__t = ( message.time )) == null ? '' : __t) +
+'</p>\n</a></li>';
 
 }
 return __p
