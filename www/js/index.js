afdf95693e9fa7d20cbd129f14dfed9c31d1c428
Peter Hoellig <phoellig@gmail.com>
2015-06-05 14:14:08 -0500

    forgot to commit web app changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+cordova.define("cordova/plugin/SmsInboxPlugin", function(require, exports, module) {
+  var exec = require('cordova/exec');
+  var SmsInboxPlugin = function() {};
+
+  /**
+   * Check if the device has a possibility to send and receive SMS
+   */
+  SmsInboxPlugin.prototype.isSupported = function(successCallback,failureCallback) {
+    return exec(successCallback, failureCallback, 'SmsInboxPlugin', 'HasSMSPossibility', []);
+  }
+
+  /**
+   * Check if the device has a possibility to send and receive SMS
+   * the successCallback function receives one string as parameter
+   * formatted such as: [phonenumber]>[message].
+   * Example: +32472345678>Hello World
+   */
+  SmsInboxPlugin.prototype.startReception = function(successCallback,failureCallback) {
+    return exec(successCallback, failureCallback, 'SmsInboxPlugin', 'StartReception', []);
+  }
+
+  /**
+   * Stop the receiving sms.
+   */
+  SmsInboxPlugin.prototype.stopReception = function(successCallback,failureCallback) {
+    return exec(successCallback, failureCallback, 'SmsInboxPlugin', 'StopReception', []);
+  }
+  
+  /**
+   * Get all existing SMS.
+   */
+  SmsInboxPlugin.prototype.getMessages = function(successCallback,failureCallback) {
+    return exec(successCallback, failureCallback, 'SmsInboxPlugin', 'getMessages', []);
+  }
+
+  var smsinboxplugin = new SmsInboxPlugin();
+  module.exports = smsinboxplugin;
+});
+
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        console.log("Attempting to resolve the plugin");
+        var smsInboxPlugin = cordova.require('cordova/plugin/SmsInboxPlugin');
+        smsInboxPlugin.getMessages(function(result) {
+        	console.log(result);
+        	app.messages = result;
+        }, function (result) {
+            console.log(result);
+        	app.errors = result;
+        });
     },
     // Bind Event Listeners
     //
@@ -43,7 +74,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+		
         console.log('Received Event: ' + id);
     }
 };
